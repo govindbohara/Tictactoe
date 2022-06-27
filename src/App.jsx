ceaa0994ee6f1d9ea9332c4fc4e6612f566923e8
govindbohara <np01cp4a190225@islingtoncollege.edu.np>
2022-06-27 14:24:29 +0545

    Created tictactoe

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TictactoeBoard from './Tictactoe/TictactoeBoard';
+
+function App() {
+	return <TictactoeBoard />;
+}
+
+export default App;
---
+++ src/Tictactoe/TictactoeBoard.jsx
@@ -0,0 +1,83 @@
+import { useState } from 'react';
+import classes from './board.module.css';
+
+const TictactoeBoard = () => {
+	const randomPlayer = ['X', 'O'];
+	const random = randomPlayer[Math.floor(Math.random() * randomPlayer.length)];
+	const [player, setPlayer] = useState(random);
+	const [winner, setWinner] = useState();
+
+	const [box, setBox] = useState(Array(9).fill(''));
+	const WINNING_COMBINATION = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+	];
+	const checkWinner = arr => {
+		WINNING_COMBINATION.forEach(item => {
+			console.log(item);
+			if (arr[item[0]] === '' || arr[item[1]] === '' || arr[item[2]] === '') {
+				console.log('hello');
+			} else if (arr[item[0]] === arr[item[1]] && arr[item[1]] === arr[item[2]]) {
+				setWinner(arr[item[0]]);
+			}
+		});
+	};
+
+	const changeHandler = num => {
+		const allValues = [...box];
+		if (allValues[num] !== '') {
+			console.log('You clicked it already');
+			return;
+		}
+
+		if (player === 'X') {
+			allValues[num] = 'X';
+			setPlayer('O');
+		} else {
+			allValues[num] = 'O';
+			setPlayer('X');
+		}
+		checkWinner(allValues);
+		setBox(allValues);
+	};
+
+	const Item = ({ num }) => {
+		return (
+			<div className={classes.item} onClick={() => changeHandler(num)}>
+				{box[num]}
+			</div>
+		);
+	};
+	return (
+		<div>
+			<h1>Player turn: {player}</h1>
+			<div className={classes.board}>
+				<Item num={0} />
+				<Item num={1} />
+				<Item num={2} />
+				<Item num={3} />
+				<Item num={4} />
+				<Item num={5} />
+				<Item num={6} />
+				<Item num={7} />
+				<Item num={8} />
+			</div>
+			{winner && <h3>Winner :{winner} wins</h3>}
+			<button
+				onClick={() => {
+					setWinner(null);
+					setBox(Array(9).fill(''));
+				}}
+			>
+				Replay the game
+			</button>
+		</div>
+	);
+};
+export default TictactoeBoard;
